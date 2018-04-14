8e6bfd367751dd6e43ee78833ca4e000d91ce419
Praaveen <praaveen.vr@spritle.com>
2018-04-14 22:51:56 +0530

    Componenets Chapter

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App, Header, Content} from './App.jsx';
+import App from './App.jsx';
 
-let modals = (
-    <div>
-        <App />
-        <Header />
-        <Content />
-    </div>
-);
 
-ReactDOM.render(modals, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
 
